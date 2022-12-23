c56be066d91e4fd4e921a6172889c715a925459f
Warren Stix <me@warrenstix.xyz>
2022-12-23 16:02:17 -0700

    basic volume with my scripts

--- blocks.h
+++ blocks.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{" ", "acpi -b | awk 'NR==1{split($0,a,\", \"); printf \"%s (%s)\", a[2], a[3]}'",	30,		0},
-
-	{"", "date '+%b %d (%a) %H:%M:%S '",					1,		0},
+	{"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+	{"^c#B8BB26^^c#BBBBBB^ ", "acpi -b | awk 'NR==1{split($0,a,\", \"); printf \"%s (%s)\", a[2], a[3]}'", 30, 0},
+	/* {" ", "acpi -b | awk 'NR==1{split($0,a,\", \"); printf \"%s (%s)\", a[2], a[3]}'", 30, 0}, */
+	{" ", "bar-vol", 0, 1},
+	{"", "date '+%b %d (%a) %H:%M:%S '", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
